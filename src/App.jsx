cc2b7ffaf89987be3b147aba09e8a07c59411494
CKMolinga <ck.molinga@gmail.com>
2022-08-15 16:56:57 +0100

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import styles from "./style";
+
+const App = () => {
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <h4>Hello world</h4>
+    </div>
+  );
+};
+
+export default App;
